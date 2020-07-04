da3c527b51fe1b9c9c822e422b05406a33f521cf
Daniel Gacitua <daniel.gacitua@usach.cl>
2020-07-04 18:57:07 -0400

    Frontend code cleanup (2)

--- client/index.js
+++ client/index.js
@@ -5,13 +5,6 @@ import VeeValidate from 'vee-validate';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-/*
-import VueAuth from '@websanova/vue-auth';
-import VueAuthBearer from '@websanova/vue-auth/drivers/auth/bearer.js';
-import VueAuthHttp from '@websanova/vue-auth/drivers/http/axios.1.x.js';
-import VueAuthRouter from '@websanova/vue-auth/drivers/router/vue-router.2.x.js';
-*/
-
 import App from './templates/App.vue';
 import { router } from './modules/routes';
 import { store } from './modules/store';
@@ -28,21 +21,6 @@ Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.use(BootstrapVue);
 
 Vue.router = router;
-
-/*
-Vue.use(VueAuth, {
-  auth: VueAuthBearer,
-  http: VueAuthHttp,
-  router: VueAuthRouter,
-  refreshData: {
-    enabled: false,
-    interval: 0
-  },
-  parseUserData: (data) => {
-    return data
-  }
-});
-*/
 
 new Vue({
   el: '#app',
