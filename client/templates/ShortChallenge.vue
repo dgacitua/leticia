eb2f84bfc1a41e41751e689bfb24a77e32cc394c
Daniel Gacitua <daniel.gacitua@usach.cl>
2020-11-07 19:08:37 -0300

    Add short challenge template

---
+++ client/templates/ShortChallenge.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Short Challenge!</h1>
+</template>
+
+<script>
+import Axios from 'axios';
+
+import * as Constants from '../services/Constants';
+import Timer from '../services/Timer';
+
+export default {
+  data() {
+    return {
+      tasks: []
+    }
+  },
+
+
+  methods: {
+    importTasks() {
+      Axios.get(`${Constants.backendApiUrl}/tasks/shuffle`)
+        .then((res) => {
+          // TODO save tasks
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
