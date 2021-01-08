12905df61b954838a855b3e6b97cbffbc289981e
Daniel Gacitua <daniel.gacitua@usach.cl>
2021-01-07 21:21:43 -0300

    Repopulate DB script

---
+++ server/scripts/repopulate.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mongo leticia-user cleanUserDatabase.js
+mongo leticia-data cleanDataDatabase.js
+
+mongo leticia-user populateUserDatabase.js
+mongo leticia-data populateDataDatabase.js
