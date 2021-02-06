95f110fc65dc245d30fcc1a0519af11530460b91
Daniel Gacitua <daniel.gacitua@usach.cl>
2021-02-06 19:59:20 -0300

    Task description for extended challenge (WIP)

---
+++ client/templates/search/TaskDescription.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <b-row>
+      <b-col>
+        <b-card :header="currentTask.title" class="text-center">
+          <b-card-text>{{ currentTask.description }}</b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
+    <br>
+    <b-row class="text-right">
+      <b-col>
+        <b-button @click="nextStage" variant="success">Continuar</b-button>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import EventBus from '../../modules/eventBus';
+
+export default {
+  data() {
+    return {
+      currentStage: this.$store.getters.stages[this.$store.getters.stageIndex],
+      currentTask: {}
+    }
+  },
+
+  computed: {
+    loggedIn() {
+      return this.$store.state.auth.status.loggedIn;
+    }
+  },
+
+  mounted() {
+    this.currentTask = this.$store.getters.tasks.find((e) => { return e.searchTaskId === this.currentStage.query.task });
+    this.$store.commit({ type: 'setCurrentTask', task: this.currentStage.query.task });
+    console.log('CurrentTask', this.currentTask, this.currentStage.query.task);
+    //let nextStage = this.$store.getters.stages[this.$store.getters.stageIndex];
+    //this.$router.replace(nextStage);
+  },
+
+  methods: {
+    nextStage() {
+      EventBus.$emit('leticia-next-challenge');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
