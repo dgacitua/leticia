f78fcea7e35432830e98afe9d602ea08f54b528d
Daniel Gacitua <daniel.gacitua@usach.cl>
2021-08-13 18:00:03 -0400

    Code cleaning

--- server/index.js
+++ server/index.js
@@ -1,13 +1,9 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-//import http from 'http';
-import path from 'path';
 import express from 'express';
-import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
 import passport from 'passport';
 
 import { dataDb, userDb } from './db';
@@ -17,16 +13,11 @@ import api1 from './api1';
 import { consoleLog, consoleError } from './utils';
 
 const app = express();
-const port = 3001;
-
-let corsOptions = {
-  origin: Constants.isProductionMode ? `${Constants.corsUrl}` : 'http://localhost:3000'
-};
+const port = 3000;
 
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
-//app.use(helmet());
 app.use(passport.initialize());
 
 // Proxy support
@@ -54,6 +45,10 @@ app.use('/v1', api1);
 // LeTiCiA Frontend
 app.get('*', express.static(Constants.frontendPath));
 
-app.listen(port, '0.0.0.0', () => consoleLog(`Backend REST API listening on port ${port}!`));
+// Deploy LeTiCiA Application
+app.listen(port, '0.0.0.0', () => consoleLog(`LeTiCiA Web App deployed on port ${port}!`));
 
+// Report LeTiCia config options
 consoleLog(`LeTiCiA Pilot Mode: ${Constants.isPilotMode}`);
+consoleLog(`LeTiCiA Asset Path: ${Constants.assetPath}`);
+consoleLog(`LeTiCiA Frontend Path: ${Constants.frontendPath}`);
