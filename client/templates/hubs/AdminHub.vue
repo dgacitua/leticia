321b23194d76075c954acb300436bd0fcd302e43
Daniel Gacitua <daniel.gacitua@usach.cl>
2021-09-09 12:56:13 -0300

    Translate Admin Hub

--- client/templates/hubs/AdminHub.vue
+++ client/templates/hubs/AdminHub.vue
@@ -6,32 +6,32 @@
     <b-row>
       <b-col>
         <b-row>
-          <h2>Etapas Estudio Corto</h2>
+          <h2>Short Challenge Stages</h2>
         </b-row>
         <b-row>
           <b-button-group vertical>
-            <b-button @click="simulateLogin">Simular Login</b-button>
-            <b-button to="consent">Consentimiento Informado</b-button>
-            <b-button to="demographic">Demográfico</b-button>
-            <b-button to="typing">Test Tipeo (baseline)</b-button>
-            <b-button to="instructions">Instrucciones Generales</b-button>
+            <b-button @click="simulateLogin">Simulate Login</b-button>
+            <b-button to="consent">Informed Consent</b-button>
+            <b-button to="demographic">Demographic Questionnaire</b-button>
+            <b-button to="typing">Typing Test (baseline)</b-button>
+            <b-button to="instructions">General Instructions</b-button>
             <b-button to="tasks">Query Planning</b-button>
-            <b-button to="exit-survey">Encuesta de Salida</b-button>
-            <b-button to="short-challenge">Desafío Corto</b-button>
-            <b-button to="consent">Experimento Corto</b-button>
+            <b-button to="exit-survey">Exit Survey</b-button>
+            <b-button to="short-challenge">Short Challenge</b-button>
+            <b-button to="consent">Full Short Challenge</b-button>
           </b-button-group>
         </b-row>
       </b-col>
       <b-col>
         <b-row>
-          <h2>Etapas Estudio Extendido</h2>
+          <h2>Extended Challenge Stages</h2>
         </b-row>
         <b-row>
           <b-button-group vertical>
-            <b-button @click="simulateLogin">Simular Login</b-button>
-            <b-button to="/extended-challenge/search">Búsqueda</b-button>
-            <b-button to="extended-challenge">Desafío Extendido</b-button>
-            <b-button to="consent">Experimento Extendido</b-button>
+            <b-button @click="simulateLogin">Simulate Login</b-button>
+            <b-button to="/extended-challenge/search">Search</b-button>
+            <b-button to="extended-challenge">Extended Challenge</b-button>
+            <b-button to="consent">Full Extended Challenge</b-button>
           </b-button-group>
         </b-row>
       </b-col>
