c98c4d5be5a4ad5efd8c9097985e00e377117c03
Daniel Gacitúa <daniel.gacitua@usach.cl>
2021-12-31 15:06:55 -0300

    JWT Key env variable fix

--- server/constants.js
+++ server/constants.js
@@ -15,7 +15,7 @@ export const solrCore = process.env.SOLR_CORE || 'leticia';
 
 export const mongoDataUrl = process.env.MONGODB_DATA_URL || 'Your MongoDB URI here';
 export const mongoUserUrl = process.env.MONGODB_USER_URL || 'Your MongoDB URI here';
-export const jwtKey = process.env.JWT_KEY || 'Your JWT key';
+export const jwtKey = process.env.JWT_KEY || 'Your JWT key';
 export const googleClientId = process.env.GOOGLE_CLIENT_ID || '';
 export const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET || '';
 export const facebookClientId = process.env.FACEBOOK_CLIENT_ID || '';
